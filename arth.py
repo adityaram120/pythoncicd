6a9d98248767c9bc73ef56de02b5033dc37e3df5
adityaram120 <adityaram1230@gmail.com>
2026-08-26 12:43:31 +0530

    Update arth.py

--- arth.py
+++ arth.py
@@ -1,10 +1,13 @@
 def add(a,b):
     return a+b
+
 def sub(a,b):
     return a-b
+
 def mul(a,b):
     return a*b
-if__name__=="_main__":
-    print("addition is",add(10,5))
-    print("Subtraction is",sub(10,5))
-    print("multiplication is ",mul(10,5))
+
+if __name__ == "__main__":
+    print("addition is", add(10,5))
+    print("Subtraction is", sub(10,5))
+    print("multiplication is", mul(10,5))
